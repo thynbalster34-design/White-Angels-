beb0caac3f28740cdde8fcd0b738b5d6101eeba1
Jan vis <thynbalster34@gmail.com>
2026-08-25 00:19:39 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         name: "Custom Status", // required by Discord API, not shown in the client
-        state: "Tuko is gay",     // this is what people actually see
+        state: "White angels 🤍",     // this is what people actually see
         type: 4,               // Custom
       },
     ],
